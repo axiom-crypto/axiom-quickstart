c6d418b20e30fe0fd551d702ff64f9096274d127
Yi Sun <yi-sun@users.noreply.github.com>
2024-01-10 00:35:38 -0500

    feat: add first working prototype of AxiomTest

--- test/AverageBalance.t.sol
+++ test/AverageBalance.t.sol
@@ -2,54 +2,68 @@
 pragma solidity ^0.8.19;
 
 import { AxiomTest } from "./AxiomTest.sol";
-import { AverageBalance } from "../src/AverageBalance.sol";
 import { IAxiomV2Query } from "axiom-v2-contracts/contracts/interfaces/query/IAxiomV2Query.sol";
 
+import { AverageBalance } from "../src/AverageBalance.sol";
+
 contract AverageBalanceTest is AxiomTest {
+    address public axiomV2QueryAddress = 0xD4E7469fdB3cAe2C85db4dacD44cb974757CbeD9;
+    IAxiomV2Query public axiomV2QueryMock;
+
     AverageBalance public averageBalance;
-    address public axiomV2QueryMock = 0xf15cc7B983749686Cd1eCca656C3D3E46407DC1f;
-    bytes32 querySchema;
-    uint64 sourceChainId = 5;
+
+    bytes32 public querySchema;
+    uint64 public sourceChainId;
+
+    string public circuitPath;
+    string public urlOrAlias;
 
     function setUp() public {
-        vm.createSelectFork("goerli");
-        vm.makePersistent(axiomV2QueryMock);
-        querySchema = _axiomCompile("app/axiom/average.circuit.ts", "goerli");
-        averageBalance = new AverageBalance(axiomV2QueryMock, sourceChainId, querySchema);
+        circuitPath = "app/axiom/average.circuit.ts";
+        urlOrAlias = "sepolia";
+        sourceChainId = 11_155_111;
+
+        vm.createSelectFork(urlOrAlias, 5_057_320);
+        vm.makePersistent(axiomV2QueryAddress);
+        axiomV2QueryMock = IAxiomV2Query(axiomV2QueryAddress);
+
+        querySchema = _axiomCompile(circuitPath, urlOrAlias);
+        averageBalance = new AverageBalance(axiomV2QueryAddress, sourceChainId, querySchema);
     }
 
     function test_axiomSendQuery() public {
-        _axiomProve("app/axiom/average.circuit.ts", "goerli", sourceChainId);
-        _axiomQuery("goerli", address(averageBalance), sourceChainId);
+        IAxiomV2Query.AxiomV2FeeData memory feeData = IAxiomV2Query.AxiomV2FeeData({
+            maxFeePerGas: 25 gwei,
+            callbackGasLimit: 1_000_000,
+            overrideAxiomQueryFee: 0
+        });
+        AxiomTest.AxiomSendQueryArgs memory args =
+            _axiomSendQuery(circuitPath, urlOrAlias, address(averageBalance), sourceChainId, feeData);
 
-        // Read args from sendQuery.json
-        string memory sendQueryJson = vm.readFile("app/axiom/data/query.json");
-        bytes memory sendQueryCalldata = vm.parseJsonBytes(sendQueryJson, ".calldata");
-        // suggested payment value, in wei
-        uint256 value = vm.parseJsonUint(sendQueryJson, ".value");
-
-        (bool success,) = axiomV2QueryMock.call{ value: value }(sendQueryCalldata);
-        require(success);
+        uint256 queryId = axiomV2QueryMock.sendQuery{ value: args.value }(
+            args.sourceChainId,
+            args.dataQueryHash,
+            args.computeQuery,
+            args.callback,
+            args.feeData,
+            args.userSalt,
+            args.refundee,
+            args.dataQuery
+        );
     }
 
-    function test_axiomFulfillQuery() public {
-        test_axiomSendQuery();
-        // testAxiomSendQuery already proved the client circuit on input.json
-        // Now we read the outputs from output.json
-        string memory runOutput = vm.readFile("app/axiom/data/output.json");
-        bytes memory computeResults = vm.parseJson(runOutput, ".computeResults");
-        bytes32[] memory axiomResults = abi.decode(computeResults, (bytes32[]));
+    function test_axiomCallback() public {
+        IAxiomV2Query.AxiomV2FeeData memory feeData = IAxiomV2Query.AxiomV2FeeData({
+            maxFeePerGas: 25 gwei,
+            callbackGasLimit: 1_000_000,
+            overrideAxiomQueryFee: 0
+        });
+        AxiomTest.AxiomFulfillCallbackArgs memory args =
+            _axiomFulfillCallback(circuitPath, urlOrAlias, address(averageBalance), sourceChainId, feeData, msg.sender);
 
-        // WARNING: Without true Axiom fulfillment, the Ethereum data used in the client circuit is UNVERIFIED.
-
-        address caller = msg.sender; // this is who initiated the query
-        // For this test we will prank the AxiomV2Query fulfillment by pranking the callback with the axiomResults.
-        // In production, this call will be done by the AxiomV2Query contract AFTER the query has been fulfilled and validated.
-        vm.prank(axiomV2QueryMock);
-        string memory sendQueryJson = vm.readFile("app/axiom/data/sendQuery.json");
-        uint256 queryId = vm.parseJsonUint(sendQueryJson, ".queryId");
-        bytes memory extraData = ""; // no extraData in this example
-
-        averageBalance.axiomV2Callback(sourceChainId, caller, querySchema, queryId, axiomResults, extraData);
+        vm.prank(axiomV2QueryAddress);
+        averageBalance.axiomV2Callback(
+            args.sourceChainId, args.caller, args.querySchema, args.queryId, args.axiomResults, args.extraData
+        );
     }
 }
--- test/AxiomTest.sol
+++ test/AxiomTest.sol
@@ -6,6 +6,27 @@ import "forge-std/Test.sol";
 import { IAxiomV2Query } from "axiom-v2-contracts/contracts/interfaces/query/IAxiomV2Query.sol";
 
 abstract contract AxiomTest is Test {
+    struct AxiomSendQueryArgs {
+        uint64 sourceChainId;
+        bytes32 dataQueryHash;
+        IAxiomV2Query.AxiomV2ComputeQuery computeQuery;
+        IAxiomV2Query.AxiomV2Callback callback;
+        IAxiomV2Query.AxiomV2FeeData feeData;
+        bytes32 userSalt;
+        address refundee;
+        bytes dataQuery;
+        uint256 value;
+    }
+
+    struct AxiomFulfillCallbackArgs {
+        uint64 sourceChainId;
+        address caller;
+        bytes32 querySchema;
+        uint256 queryId;
+        bytes32[] axiomResults;
+        bytes extraData;
+    }
+
     function _axiomCompile(string memory circuitPath, string memory urlOrAlias)
         internal
         returns (bytes32 querySchema)
@@ -30,12 +51,10 @@ abstract contract AxiomTest is Test {
         querySchema = bytes32(vm.parseJson(artifact, ".querySchema"));
     }
 
-    function _axiomProve(
-        string memory circuitPath,
-        string memory urlOrAlias,
-        uint64 sourceChainId,
-        IAxiomV2Query.AxiomV2FeeData memory feeData
-    ) internal returns (bytes memory output) {
+    function _axiomProve(string memory circuitPath, string memory urlOrAlias, uint64 sourceChainId)
+        internal
+        returns (bytes memory output)
+    {
         string[] memory cli = new string[](18);
         cli[0] = "npx";
         cli[1] = "axiom";
@@ -58,34 +77,114 @@ abstract contract AxiomTest is Test {
         output = vm.ffi(cli);
     }
 
-    function _axiomQuery(string memory urlOrAlias, address callback, uint64 sourceChainId)
-        internal
-        returns (bytes memory output)
-    {
-        string[] memory cli = new string[](23);
+    function _axiomQueryParams(
+        string memory urlOrAlias,
+        address callback,
+        uint64 sourceChainId,
+        IAxiomV2Query.AxiomV2FeeData memory feeData
+    ) internal {
+        string[] memory cli = new string[](22);
         cli[0] = "npx";
         cli[1] = "axiom";
         cli[2] = "circuit";
         cli[3] = "query-params";
         cli[4] = vm.toString(callback); // the callback target address
-        cli[5] = "--mock";
-        cli[6] = "--sourceChainId";
-        cli[7] = vm.toString(sourceChainId);
-        cli[8] = "--refundAddress";
-        cli[9] = vm.toString(msg.sender);
-        cli[10] = "--callbackExtraData";
-        cli[11] = "0x"; // no extraData in this example
-        cli[12] = "--maxFeePerGas";
-        cli[13] = "0";
-        cli[14] = "--callbackGasLimit";
-        cli[15] = "0";
-        cli[16] = "true";
-        cli[17] = "--provider";
-        cli[18] = vm.rpcUrl(urlOrAlias);
-        cli[19] = "--input";
-        cli[20] = "app/axiom/data/output.json";
-        cli[21] = "--output";
-        cli[22] = "app/axiom/data/query.json";
-        output = vm.ffi(cli);
+        cli[5] = "--sourceChainId";
+        cli[6] = vm.toString(sourceChainId);
+        cli[7] = "--refundAddress";
+        cli[8] = vm.toString(msg.sender);
+        cli[9] = "--callbackExtraData";
+        cli[10] = "0x"; // no extraData in this example
+        cli[11] = "--maxFeePerGas";
+        cli[12] = vm.toString(feeData.maxFeePerGas);
+        cli[13] = "--callbackGasLimit";
+        cli[14] = vm.toString(feeData.callbackGasLimit);
+        cli[15] = "--provider";
+        cli[16] = vm.rpcUrl(urlOrAlias);
+        cli[17] = "--input";
+        cli[18] = "app/axiom/data/output.json";
+        cli[19] = "--output";
+        cli[20] = "app/axiom/data/query.json";
+        cli[21] = "--calldata";
+        vm.ffi(cli);
+    }
+
+    function _axiomSendQuery(
+        string memory circuitPath,
+        string memory urlOrAlias,
+        address callback,
+        uint64 sourceChainId,
+        IAxiomV2Query.AxiomV2FeeData memory feeData
+    ) internal returns (AxiomSendQueryArgs memory args) {
+        _axiomProve(circuitPath, urlOrAlias, sourceChainId);
+        _axiomQueryParams(urlOrAlias, callback, sourceChainId, feeData);
+        string memory filePath = "app/axiom/data/query.json";
+        bytes memory calldataBytes = abi.decode(vm.parseJson(vm.readFile(filePath), ".calldata"), (bytes));
+
+        // use of `this` is necessary to convert memory to calldata and allow slicing
+        args = this._parseSendQueryArgs(calldataBytes);
+        args.value = vm.parseJsonUint(vm.readFile(filePath), ".value");
+    }
+
+    function _axiomFulfillCallback(
+        string memory circuitPath,
+        string memory urlOrAlias,
+        address callback,
+        uint64 sourceChainId,
+        IAxiomV2Query.AxiomV2FeeData memory feeData,
+        address caller
+    ) internal returns (AxiomFulfillCallbackArgs memory args) {
+        _axiomProve(circuitPath, urlOrAlias, sourceChainId);
+        _axiomQueryParams(urlOrAlias, callback, sourceChainId, feeData);
+
+        AxiomSendQueryArgs memory sendQueryArgs = this._parseSendQueryArgs(
+            abi.decode(vm.parseJson(vm.readFile("app/axiom/data/query.json"), ".calldata"), (bytes))
+        );
+        args = AxiomFulfillCallbackArgs({
+            sourceChainId: sourceChainId,
+            caller: caller,
+            querySchema: abi.decode(vm.parseJson(vm.readFile("app/axiom/data/compiled.json"), ".querySchema"), (bytes32)),
+            queryId: vm.parseJsonUint(vm.readFile("app/axiom/data/query.json"), ".queryId"),
+            axiomResults: abi.decode(
+                vm.parseJson(vm.readFile("app/axiom/data/output.json"), ".computeResults"), (bytes32[])
+                ),
+            extraData: sendQueryArgs.callback.extraData
+        });
+    }
+
+    function _parseSendQueryArgs(bytes calldata calldataBytes) public returns (AxiomSendQueryArgs memory args) {
+        (
+            uint64 sourceChainId,
+            bytes32 dataQueryHash,
+            IAxiomV2Query.AxiomV2ComputeQuery memory computeQuery,
+            IAxiomV2Query.AxiomV2Callback memory callback,
+            IAxiomV2Query.AxiomV2FeeData memory feeData,
+            bytes32 userSalt,
+            address refundee,
+            bytes memory dataQuery
+        ) = abi.decode(
+            calldataBytes[4:],
+            (
+                uint64,
+                bytes32,
+                IAxiomV2Query.AxiomV2ComputeQuery,
+                IAxiomV2Query.AxiomV2Callback,
+                IAxiomV2Query.AxiomV2FeeData,
+                bytes32,
+                address,
+                bytes
+            )
+        );
+        args = AxiomSendQueryArgs({
+            sourceChainId: sourceChainId,
+            dataQueryHash: dataQueryHash,
+            computeQuery: computeQuery,
+            callback: callback,
+            feeData: feeData,
+            userSalt: userSalt,
+            refundee: refundee,
+            dataQuery: dataQuery,
+            value: 0
+        });
     }
 }
