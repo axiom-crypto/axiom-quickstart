04fe7581d5216ccea19d3710822fd8cde60da487
roshan <19766713+rpalakkal@users.noreply.github.com>
2024-01-11 12:26:14 -0500

    feat: update to new v2-client

--- test/AverageBalance.t.sol
+++ test/AverageBalance.t.sol
@@ -7,19 +7,10 @@ import { IAxiomV2Query } from "@axiom-v2-client/interfaces/query/IAxiomV2Query.s
 import { AverageBalance } from "../src/AverageBalance.sol";
 
 contract AverageBalanceTest is AxiomTest {
-    address public axiomV2QueryAddress = 0xD4E7469fdB3cAe2C85db4dacD44cb974757CbeD9;
-    IAxiomV2Query public axiomV2QueryMock;
-
     AverageBalance public averageBalance;
 
-    bytes32 public querySchema;
-    uint64 public sourceChainId;
-
-    string public circuitPath;
-    string public urlOrAlias;
-
     function setUp() public {
-        circuitPath = "app/axiom/average.circuit.ts";
+        circuitPath = "src/average.circuit.ts";
         urlOrAlias = "sepolia";
         sourceChainId = 11_155_111;
 
@@ -32,13 +23,9 @@ contract AverageBalanceTest is AxiomTest {
     }
 
     function test_axiomSendQuery() public {
-        IAxiomV2Query.AxiomV2FeeData memory feeData = IAxiomV2Query.AxiomV2FeeData({
-            maxFeePerGas: 25 gwei,
-            callbackGasLimit: 1_000_000,
-            overrideAxiomQueryFee: 0
-        });
-        AxiomTest.AxiomSendQueryArgs memory args =
-            _axiomSendQuery(circuitPath, urlOrAlias, address(averageBalance), sourceChainId, feeData);
+        AxiomTest.AxiomSendQueryArgs memory args = _axiomSendQuery(
+            circuitPath, inputPath, urlOrAlias, address(averageBalance), callbackExtraData, sourceChainId, feeData
+        );
 
         uint256 queryId = axiomV2QueryMock.sendQuery{ value: args.value }(
             args.sourceChainId,
@@ -53,17 +40,16 @@ contract AverageBalanceTest is AxiomTest {
     }
 
     function test_axiomCallback() public {
-        IAxiomV2Query.AxiomV2FeeData memory feeData = IAxiomV2Query.AxiomV2FeeData({
-            maxFeePerGas: 25 gwei,
-            callbackGasLimit: 1_000_000,
-            overrideAxiomQueryFee: 0
-        });
-        AxiomTest.AxiomFulfillCallbackArgs memory args =
-            _axiomFulfillCallback(circuitPath, urlOrAlias, address(averageBalance), sourceChainId, feeData, msg.sender);
-
-        vm.prank(axiomV2QueryAddress);
-        averageBalance.axiomV2Callback(
-            args.sourceChainId, args.caller, args.querySchema, args.queryId, args.axiomResults, args.extraData
+        AxiomTest.AxiomFulfillCallbackArgs memory args = _axiomFulfillCallback(
+            circuitPath,
+            inputPath,
+            urlOrAlias,
+            address(averageBalance),
+            callbackExtraData,
+            sourceChainId,
+            feeData,
+            msg.sender
         );
+        _axiomPrankCallback(args);
     }
 }
