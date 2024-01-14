4193da22da5559b2f4c66a4f79b670bcae6881fd
Yu Jiang Tham <yoojbruin@gmail.com>
2024-01-13 19:51:26 -0800

    feat: merge

--- app/index.ts
+++ app/index.ts
@@ -1,80 +1,35 @@
+import { circuit, CircuitInputs } from "./axiom/average.circuit";
+import { Axiom } from "@axiom-crypto/client";
 import dotenv from "dotenv";
 dotenv.config();
-import { CircuitInputs, circuit } from "./axiom/average.circuit";
-import { ethers } from "ethers";
-import { AxiomCircuit } from "@axiom-crypto/client/js";
 
-// Compiled circuit file after running `npx axiom circuit compile app/axiom/average.circuit.ts`
-import compiledJson from "./axiom/data/compiled.json";
+// Compiled circuit file after running:
+//   `npx axiom circuit compile app/axiom/average.circuit.ts`
+import compiledCircuit from "./axiom/data/compiled.json";
 
-// TMP: until we expose abi in AxiomCircuit
-import axiomV2QueryJson from "./axiom/abi/AxiomV2Query.json";
-
-const axiomMain = async () => {
-  const axiomCircuit = new AxiomCircuit({
-    provider: process.env.PROVIDER_URI_SEPOLIA as string,
-    f: circuit,
-    inputSchema: `{
-      "blockNumber": "CircuitValue",
-      "address": "CircuitValue"
-    }`,
-    chainId: "11155111", // Sepolia
-    mock: false,
-  });
-  const defaultInputs = {
-    blockNumber: 5000000,
-    address: "0xEaa455e4291742eC362Bc21a8C46E5F2b5ed4701"
-  };
-  // const artifact = await axiomCircuit.compile(defaultInputs);
-
-  // Instead of compiling, you can load from a saved artifact:
-  await axiomCircuit.loadSaved({ config: compiledJson.config, vk: compiledJson.vk });
-  await axiomCircuit.run(defaultInputs);
-  console.log("Ran circuit:", axiomCircuit);
-
-  const provider = new ethers.JsonRpcProvider(
-    process.env.PROVIDER_URI_SEPOLIA as string,
-  );
-  const signer = new ethers.Wallet(
-    process.env.PRIVATE_KEY_SEPOLIA as string,
-    provider,
-  );
-
-  const axiomV2QueryAddr = "0x8ec7b212a983b1ebbfacfd69794ef179da1db0e0";
-  const deployedCallbackAddr = "0x752056074aceabac231801cbfa68900744eebc98";
-  const senderAddress = await signer.getAddress();
-
-  const sendQueryArgs = await axiomCircuit.getSendQueryArgs({
-    callbackTarget: deployedCallbackAddr,
-    callbackExtraData: ethers.toBeHex("0x00", 32),
-    callerAddress: senderAddress,
-    options: { 
-      maxFeePerGas: "50000000000",
-      refundee: senderAddress 
-    },
-  });
-
-  console.log("SendQuery args:", sendQueryArgs);
-
-  const axiomV2QueryMock = new ethers.Contract(
-    axiomV2QueryAddr,
-    axiomV2QueryJson.abi,
-    signer,
-  );
-
-  console.log(
-    "Sending a Query to AxiomV2QueryMock with payment amount (wei):",
-    sendQueryArgs.value,
-  );
-
-  const tx = await axiomV2QueryMock.sendQuery(...sendQueryArgs.args, { value: sendQueryArgs.value });
-  const receipt: ethers.ContractTransactionReceipt = await tx.wait();
-  console.log("Transaction receipt:", receipt);
-
-  console.log(
-    "View your Query on Axiom Explorer (currently not implemented for Sepolia):",
-    `https://explorer.axiom.xyz/v2/sepolia/mock/query/${sendQueryArgs.queryId}`,
-  );
+const exampleInput: CircuitInputs = {
+  blockNumber: 5000000,
+  address: "0xEaa455e4291742eC362Bc21a8C46E5F2b5ed4701"
 };
 
-axiomMain();
+const axiomMain = async (input: CircuitInputs) => {
+  const axiom = new Axiom({
+    circuit: circuit,
+    compiledCircuit: compiledCircuit,
+    inputSchema: {
+      blockNumber: "uint32",
+      address: "address",
+    },
+    chainId: "11155111",  // Sepolia
+    provider: process.env.PROVIDER_URI_SEPOLIA as string,
+    privateKey: process.env.PRIVATE_KEY_SEPOLIA as string,
+    mock: false,
+  });
+  await axiom.init();
+
+  const args = await axiom.prove(input);
+  const receipt = await axiom.sendQuery(args);
+  console.log("Transaction receipt:", receipt);
+};
+
+axiomMain(exampleInput);
