dfb1579b0dd76a8a7dc7724d80be5b7b6dbb4470
Yu Jiang Tham <yoojbruin@gmail.com>
2024-06-12 13:55:03 -0400

    Split samechain and crosschain

--- app/index.ts
+++ app/index.ts
@@ -1,4 +1,4 @@
-import { Axiom, AxiomCrosschain, BridgeType, UserInput } from '@axiom-crypto/client';
+import { Axiom, UserInput } from '@axiom-crypto/client';
 import { circuit, CircuitInputs } from "./axiom/average.circuit";
 import { chainIdToPathname } from "./utils";
 import dotenv from "dotenv";
@@ -11,61 +11,38 @@ import inputs from './axiom/data/inputs.json';
 //   `npx axiom circuit compile app/axiom/average.circuit.ts`
 import compiledCircuit from "./axiom/data/compiled.json";
 
-const SOURCE_CHAIN_ID = "11155111";
-const TARGET_CHAIN_ID = "84532";
-const SOURCE_RPC_URL = process.env[`RPC_URL_${SOURCE_CHAIN_ID}`];
-const TARGET_RPC_URL = process.env[`RPC_URL_${TARGET_CHAIN_ID}`];
+const CHAIN_ID = "11155111";
+const RPC_URL = process.env[`RPC_URL_${CHAIN_ID}`];
 
-if (!SOURCE_RPC_URL || !TARGET_RPC_URL) {
-  console.error(`RPC URLs must be provided for env vars \`RPC_URL_${SOURCE_CHAIN_ID}\` and \`RPC_URL_${TARGET_CHAIN_ID}\`.`);
+if (!RPC_URL) {
+  console.error(`RPC URL must be provided for env var \`RPC_URL_${CHAIN_ID}\`.`);
   process.exit(1);
 }
 
 const axiomMain = async (input: UserInput<CircuitInputs>) => {
-  // const axiom = new Axiom({
-  //   circuit: circuit,
-  //   compiledCircuit: compiledCircuit,
-  //   chainId: CHAIN_ID,  // Sepolia
-  //   rpcUrl: process.env[`RPC_URL_${CHAIN_ID}`] as string,
-  //   privateKey: process.env[`PRIVATE_KEY_${CHAIN_ID}`] as string,
-  //   callback: {
-  //     target: "0x4A4e2D8f3fBb3525aD61db7Fc843c9bf097c362e",
-  //   },
-  //   options: {
-  //     // maxFeePerGas: "50000",
-  //     overrideAxiomQueryFee: "3000000000000001",
-  //   }
-  // });
-  const axiom = new AxiomCrosschain({
-    circuit,
-    compiledCircuit,
-    source: {
-      chainId: SOURCE_CHAIN_ID,
-      rpcUrl: SOURCE_RPC_URL,
-    },
-    target: {
-      chainId: TARGET_CHAIN_ID,
-      rpcUrl: TARGET_RPC_URL,
-      privateKey: process.env[`PRIVATE_KEY_${TARGET_CHAIN_ID}`] as string,
-    },
-    bridgeType: BridgeType.BlockhashOracle,
+  const axiom = new Axiom({
+    circuit: circuit,
+    compiledCircuit: compiledCircuit,
+    chainId: CHAIN_ID,  // Sepolia
+    rpcUrl: process.env[`RPC_URL_${CHAIN_ID}`] as string,
+    privateKey: process.env[`PRIVATE_KEY_${CHAIN_ID}`] as string,
     callback: {
       target: "0x4A4e2D8f3fBb3525aD61db7Fc843c9bf097c362e",
     },
   });
   await axiom.init();
   const args = await axiom.prove(input);
-  console.log("ZK proof generated successfully., args: ", args.args);
+  console.log("ZK proof generated successfully.");
 
-  if (!process.env[`PRIVATE_KEY_${TARGET_CHAIN_ID}`]) {
-    console.log(`No private key provided for env var \`PRIVATE_KEY_${TARGET_CHAIN_ID}\`: Query will not be sent to the blockchain.`);
+  if (!process.env[`PRIVATE_KEY_${CHAIN_ID}`]) {
+    console.log(`No private key provided for env var \`PRIVATE_KEY_${CHAIN_ID}\`: Query will not be sent to the blockchain.`);
     return;
   }
 
   console.log("Sending Query to Axiom on-chain...");
   const receipt = await axiom.sendQuery();
   console.log("Transaction receipt:", receipt);
-  console.log(`View your Query on Axiom Explorer: https://explorer.axiom.xyz/v2/${chainIdToPathname(TARGET_CHAIN_ID)}/${chainIdToPathname(SOURCE_CHAIN_ID)}/query/${args.queryId}`);
+  console.log(`View your Query on Axiom Explorer: https://explorer.axiom.xyz/v2/${chainIdToPathname(CHAIN_ID)}/query/${args.queryId}`);
 };
 
 axiomMain(inputs);
